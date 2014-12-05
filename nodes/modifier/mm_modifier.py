627b27d924ec2bb50de650f15607d3150318bbd6
vvFiCKvv <ferrari@vvFiCKvv.vv>
2014-12-05 18:04:10 +0200

    fix a lot of things(the documentatios is bad), cleenup will follow

--- nodes/modifier/mm_modifier.py
+++ nodes/modifier/mm_modifier.py
@@ -2,7 +2,8 @@ import bpy
 from bpy.types import Node
 from animation_nodes.mn_node_base import AnimationNode
 from animation_nodes.mn_execution import nodePropertyChanged, nodeTreeChanged, allowCompiling, forbidCompiling
-	
+from animation_nodes.mn_utils import *
+
 class mn_Modifier(Node, AnimationNode):
 	bl_idname = "mn_Modifier"
 	bl_label = "Modifier Output Node"
@@ -65,46 +66,72 @@ class mn_Modifier(Node, AnimationNode):
 		return
 	
 	def loadModifierProperties(self, modifier):
-		if modifier.type != self.modifierType:
-			self.initModifier(modifier)
-			
-		objName = self.inputs["Modifier"].objectName
-		modName = self.inputs["Modifier"].modifierName
 		
-		for inputSocket in self.inputs:
-			if(inputSocket.name=="Modifier"):
-				continue
-
-			if not inputSocket.is_linked:
-				continue
+		
+		for link in self.links:
+			inputSocket = link.to_socket
+			fromSocket = getOriginSocket(inputSocket)
+			inputSocket.setStoreableValue(fromSocket.getStoreableValue())
+#		objName = self.inputs["Modifier"].objectName
+#		modName = self.inputs["Modifier"].modifierName
+#		
+#		for inputSocket in self.inputs:
+#			if(inputSocket.name=="Modifier"):
+#				continue
+#
+#			if not inputSocket.is_linked:
+#				continue
 #TODO: To be removed
-			print("bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]" + " = " + str(inputSocket.getValue()))
-#			exec("bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]" + " = " + str(inputSocket.getValue()))
+#			print(inputSocket.links[0].from_node.inputs[0].getStoreableValue())
+#			inputSocket.setValue(inputSocket.links[0].from_socket.getStoreableValue())
 		return
 
-#TODO: Fix names
-	def getInputSocketNames(self):
-		return {"Modifier" : "modifier",
-				"levels" : "levels" }
-	def getOutputSocketNames(self):
-		return {"Modifier" : "modifier"}
-	
-	
-	def useInLineExecution(self):
-		return True
-	def getInLineExecutionString(self, useOutput):
-		codeLines = []
+	def execute(self,inputs):
+#		forbidCompiling()
 		objName = self.inputs["Modifier"].objectName
 		modName = self.inputs["Modifier"].modifierName
 		if not objName:
-			return "\n".join(codeLines)
+			return
 		if not modName:
-			return "\n".join(codeLines)
-		self.loadModifierProperties(eval("bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]"))
-		if useOutput["Modifier"]:
-			print("$modifier$ = bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]")
-			codeLines.append("$modifier$ = bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]")
-		return "\n".join(codeLines)
+			return
+		output = {}
+		modifier = self.inputs["Modifier"].getValue()
+		if modifier.type != self.modifierType:
+			self.initModifier(modifier)
+#		self.loadModifierProperties()
+		for input in inputs:
+			if(isSocketLinked(self.inputs[input])):
+#				print("input: ", self.inputs[input], "new input: ", str(inputs[input]))
+				self.inputs[input].setStoreableValue(str(inputs[input]))
+#			print(input)
+#		allowCompiling()
+
+		output["Modifier"] =  inputs["Modifier"]
+		return output
+
+#TODO: Fix names
+#	def getInputSocketNames(self):
+#		return {"Modifier" : "modifier",
+#				"levels" : "levels" }
+#	def getOutputSocketNames(self):
+#		return {"Modifier" : "modifier"}
+#	
+#	
+#	def useInLineExecution(self):
+#		return True
+#	def getInLineExecutionString(self, useOutput):
+#		codeLines = []
+#		objName = self.inputs["Modifier"].objectName
+#		modName = self.inputs["Modifier"].modifierName
+#		if not objName:
+#			return "\n".join(codeLines)
+#		if not modName:
+#			return "\n".join(codeLines)
+#		self.loadModifierProperties(eval("bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]"))
+#		if useOutput["Modifier"]:
+#			print("$modifier$ = bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]")
+#			codeLines.append("$modifier$ = bpy.data.objects[\""+objName+"\"].modifiers[\""+modName+"\"]")
+#		return "\n".join(codeLines)
 
 #	def execute(self, modifier):
 #		if(modifier):
